49dc2773c05856ff7e5ac0f5cbdb746d779f6fa4
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-06-29 16:04:36 -0700

    index.js updated

---
+++ models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Inventory: require("./inventory"),
+    Patient: require("./patient"),
+    Practicioner: require("./practicioner"),
+    Procedure: require("./procedure"),
+    Staff: require("./staff")
+  };
+  
