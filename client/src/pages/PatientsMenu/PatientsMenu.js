06342cea5b52c65e288615f7fab2175a0ebec8d7
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-05 18:10:11 -0700

    basic structure for all pages corrected and recreated

---
+++ client/src/pages/PatientsMenu/PatientsMenu.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+// import API from "../../utils/API"
+import Wrapper from "../../components/Wrapper/Wrapper";
+
+
+class PatientsMenu extends Component {
+    state = {
+      item: {}
+    };
+  
+    render() {
+      return (
+        <Wrapper>
+        
+        </Wrapper>
+      );
+    }
+  }
+  
+  export default PatientsMenu;
---
+++ client/src/pages/UpdateAccount/UpdateAccount.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+// import API from "../../utils/API"
+import Wrapper from "../../components/Wrapper/Wrapper";
+
+
+class UpdateAccount extends Component {
+    state = {
+      item: {}
+    };
+  
+    render() {
+      return (
+        <Wrapper>
+        
+        </Wrapper>
+      );
+    }
+  }
+  
+  export default UpdateAccount;
