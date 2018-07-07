7d86de3217b74f24b9c2e07924e5a9edd60ec2d5
Jose Salamanca <jsalamanca228@gmail.com>
2018-07-07 02:26:28 -0700

    view buttons

---
+++ client/src/components/ViewPatientsBtn/ViewPatientsBtn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ViewPatientsBtn.css";
+
+const ViewPatientsBtn = props => (
+  <span className="viewpatients-btn" {...props}>
+    View Patients
+  </span>
+);
+
+    
---
+++ client/src/components/ViewProceduresBtn/ViewProceduresBtn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ViewProceduresBtn.css";
+
+const ViewProceduresBtn = props => (
+  <span className="viewprocedures-btn" {...props}>
+    View Procedures
+  </span>
+);
+
+    
