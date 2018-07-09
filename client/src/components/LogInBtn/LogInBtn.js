f9b23e706597e80e4022ac045371f32e2b4177b4
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-09 12:11:28 -0700

    button links updated

--- client/src/components/LogInBtn/LogInBtn.js
+++ client/src/components/LogInBtn/LogInBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
  
 const LogInBtn = () => (
-  <button type="button" className="btn btn-outline-info" href="/signin">Log In</button>
+  <button type="button" className="btn btn-outline-info" onClick={( window.location = '/signin')}>Log In</button>
 );
 
 export default LogInBtn;
