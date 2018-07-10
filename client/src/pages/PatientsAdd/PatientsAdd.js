40d237038fe1c592706b607260f5c50317bef3db
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-09 17:47:31 -0700

    Website rendering, patientAdd still needs to be fixed completely

--- client/src/pages/PatientsAdd/PatientsAdd.js
+++ client/src/pages/PatientsAdd/PatientsAdd.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
-import API from "../../utils/API"
-import SearchForm from "../../components/SearchForm/SearchForm";
+// import API from "../../utils/API";
+// import SearchForm from "../../components/SearchForm/SearchForm";
 import Wrapper from "../../components/Wrapper/Wrapper";
-import PatientsFormInput from "../../components/PatientsFormInput/PatientsFormInput";
-import SubmitPatientsBtn from  "../../components/SubmitPatientsBtn/SubmitPatientsBtn";
+// import PatientsFormInput from "../../components/PatientsFormInput/PatientsFormInput";
+// import SubmitPatientsBtn from  "../../components/SubmitPatientsBtn/SubmitPatientsBtn";
 import UserForm from "../../components/UserForm/UserForm";
 
 import API from "../../utils/API";
@@ -98,7 +98,7 @@ class PatientsAdd extends Component {
     let patientResults = "Please enter all fields."
     if(this.state.showResults){
       patientResults = this.state.results.map((patient, index) =>{
-//         return <List key={patient._id} name={patient.name} age={patient.age} weight={patient.weight} height={patient.height} temperature={patient.temperature} pulse={patient.pulse} respiratory={patient.respiratory} pressure={patient.pressure} symptoms={patient.symptoms} diagnosis={patient.diagnosis} drugs={patient.drugs} inDate={patient.inDate} outDate={patient.outDate} action={this.handlePatientSaved} title="Save" />
+        return <List key={patient._id} name={patient.name} age={patient.age} weight={patient.weight} height={patient.height} temperature={patient.temperature} pulse={patient.pulse} respiratory={patient.respiratory} pressure={patient.pressure} symptoms={patient.symptoms} diagnosis={patient.diagnosis} drugs={patient.drugs} inDate={patient.inDate} outDate={patient.outDate} action={this.handlePatientSaved} title="Save" />
       });
     }
       return (
@@ -111,7 +111,7 @@ class PatientsAdd extends Component {
     }
   }
 
-}
+
   
   export default PatientsAdd;
 
