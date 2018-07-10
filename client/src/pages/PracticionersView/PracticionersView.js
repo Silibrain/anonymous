49129e4c60221d4747392e25e01dfbff818b4029
Admin <cbarnettmcms@gmail.com>
2018-07-10 13:11:13 -0700

    add add

--- client/src/pages/PracticionersView/PracticionersView.js
+++ client/src/pages/PracticionersView/PracticionersView.js
@@ -1,21 +1,46 @@
 import React, { Component } from "react";
-// import API from "../../utils/API"
+import API from "../../utils/PracticionersAPI"
 import Wrapper from "../../components/Wrapper/Wrapper";
 import List from  "../../components/List/List";
+import Jumbotron from "../../components/Jumbotron/Jumbotron";
 
 
 class PracticionersView extends Component {
-    state = {
-      item: {}
-    };
+  state = {
+    practicioner: []
+  };  
+
+  componentDidMount(){
+      this.getPracticionersHandler();
+    }
   
+    getPracticionersHandler(){
+      API.getPracticioner()
+      .then(res => this.setState({practicioner: res.data}))
+      .catch(err => console.log(err));
+    }
+  
+    deletePracticionerHandler = (event, id) => {
+      API.deletePracticioner(id)
+      .then(res => this.getPracticionersHandler())
+      .catch(err => console.log(err));
+    }
+
     render() {
+      let saved = <p> Currently, no Practicioners to Display!</p>
+  
+      if(this.state.practicioner.length > 0){
+        saved = this.state.practicioner.map((practicioner, index) => {
+          return <List key={practicioner._id} name={practicioner.name} age={practicioner.specialties} weight={practicioner.skills} height={practicioner.fees} temperature={practicioner.bio} pulse={practicioner.entryDate} respiratory={practicioner.exitDate} action={this.handlePracticionerSaved} title="Save" />
+        });
+      }
+  
       return (
         <Wrapper>
-        <List></List>
-        </Wrapper>
-      );
+          <Jumbotron title="Practicioner List"></Jumbotron>
+          {saved}
+          </Wrapper>
+      ) 
     }
   }
-  
   export default PracticionersView;
--- client/src/pages/ProceduresAdd/ProceduresAdd.js
+++ client/src/pages/ProceduresAdd/ProceduresAdd.js
@@ -1,23 +1,99 @@
 import React, { Component } from "react";
-// import API from "../../utils/API"
+// import API from "../../utils/API";
+// import SearchForm from "../../components/SearchForm/SearchForm";
 import Wrapper from "../../components/Wrapper/Wrapper";
-import ProceduresFormInput from "../../components/ProceduresFormInput/ProceduresFormInput";
-import SubmitProceduresBtn from "../../components/SubmitProceduresBtn/SubmitProceduresBtn";
+// import PatientsFormInput from "../../components/PatientsFormInput/PatientsFormInput";
+// import SubmitPatientsBtn from  "../../components/SubmitPatientsBtn/SubmitPatientsBtn";
+import UserForm from "../../components/UserForm/UserForm";
+import API from "../../utils/ProceduresAPI";
+import Jumbotron from "../../components/Jumbotron/Jumbotron";
+import List from "../../components/List/List";
 
 class ProceduresAdd extends Component {
-    state = {
-      item: {}
-    };
-  
-    render() {
-      return (
-        <Wrapper>
-        <ProceduresFormInput></ProceduresFormInput>
-        <SubmitProceduresBtn></SubmitProceduresBtn>
-        </Wrapper>
-      );
-    }
+  state = {
+    labels: [
+      {id: "Name", val:""},
+      {id: "Type", val:""},
+      {id: "Location", val: ""},
+      {id: "Result", val:""},
+      {id: "Average Time", val:""},
+      {id: "Capex", val: ""},
+      {id: "Opex", val:""},
+      {id: "Entry Date", val:""},
+      {id: "Exit Date", val:""},
+    ],
+    results:[],
+    show: false,
+    error:""
+  };
+
+  handleInputChange = ( event, id ) => {
+    const procedureIndex = this.state.procedures.findIndex(procedure => procedure.id === id);
+    const procedure = { ...this.state.procedures[procedureIndex] };
+    procedure.val = event.target.value;
+    const procedures = [ ...this.state.procedures ];
+    procedures[procedureIndex] = procedure;
+    this.setState({ procedures: procedures });
   }
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    API.getprocedure(this.state.item[0].val, this.state.item[1].val, this.state.item[2].val, this.state.item[3].val, this.state.item[4].val, this.state.item[5].val, this.state.item[6].val, this.state.item[7].val, this.state.item[8].val, this.state.item[9].val, this.state.item[10].val, this.state.item[11].val, this.state.item[12].val).then(res => {
+      this.setState({
+        labels: [
+        {id: "Name", val:""},
+        {id: "Type", val:""},
+        {id: "Location", val: ""},
+        {id: "Result", val:""},
+        {id: "Average Time", val:""},
+        {id: "Capex", val: ""},
+        {id: "Opex", val:""},
+        {id: "Entry Date", val:""},
+        {id: "Exit Date", val:""},
+        ],
+        results: res.data.response.docs,
+        showResults: true
+      });
+    })
+    .catch(err => this.setState({ error: err.message}));
+  }
+
+  handleprocedureSaved = (event, id) => {
+    event.preventDefault();
+    const procedureIndex = this.state.results.findIndex(result => result._id === id);
+    const procedure = { ...this.state.results[procedureIndex]};
+    console.log(procedure);
+    API.saveProcedure({
+      name: procedure.name,
+      type: procedure.specialties,
+      location: procedure.skills,
+      result: procedure.fees,
+      avgtime: procedure.bio,
+      capex: procedure.entryDate,
+      opex: procedure.exitDate,
+      entryDate: procedure.exitDate,
+      exitDate: procedure.exitDate,
+    })
+    .then(res => alert('Procedure saved!'))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+  let procedureResults = "Please enter all fields."
+  if(this.state.showResults){
+    procedureResults = this.state.results.map((procedure, index) =>{
+      return <List key={procedure._id} name={procedure.name} age={procedure.type} weight={procedure.location} height={procedure.result} temperature={procedure.avgtime} pulse={procedure.capex} respiratory={procedure.opex} respiratory={procedure.entryDate} respiratory={procedure.exitDate} action={this.handleProcedureSaved} title="Save" />
+    });
+  }
+    return (
+      <Wrapper>
+      <Jumbotron title="Add Procedure"></Jumbotron>
+      <UserForm submit={this.handleFormSubmit} changed={this.handleInputChange} labels={this.state.labels} />
+      {/* <proceduresView title ="procedure Results">{procedureResults}</proceduresView> */}
+      </Wrapper>
+    );
+  }
+}
   
   export default ProceduresAdd;
   
