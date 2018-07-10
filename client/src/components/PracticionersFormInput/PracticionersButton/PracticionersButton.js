1a8d7ea8aa8d37f6c3fc03201485f0c0248a881d
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-10 10:48:48 -0700

    reduced components to only those needed

--- client/src/components/PracticionersFormInput/PracticionersButton/PracticionersButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const PracticionersButton = (props) => (
-    <button onClick={props.clicked} className="btn btn-primary">
-    Submit
-    </button>
-);
-
-export default PracticionersButton;
