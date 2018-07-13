ffa6d8ea5327b18395f2a1546203d960f6672e21
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-13 10:08:09 -0700

    quickbooks added

--- client/src/components/Panels/ReportContainer.js
+++ client/src/components/Panels/ReportContainer.js
@@ -1,10 +1,39 @@
+
 import React from "react";
-import "./style.css";
+/**
+ * Connect to QBO Button
+ * @param props
+ * @returns {XML}
+ */
+const ReportContainer = props => {
+  const { callbackURL } = props;
 
-export const ReportsContainer = ({ fluid, children }) => (
-  <div className={`container${fluid ? "-fluid" : ""}`}>
-    {children}
-  </div>
-);
+  const QBLogin = () => {
+    // we wrap QBO's button in a template string
+    // becuase it has some stuff that isn't supported by JSX and react
+    // like xml name spaces
+    const template = `
+        <script type="text/javascript" src="https://appcenter.intuit.com/Content/IA/intuit.ipp.anywhere.js"></script>
+         <script>
+         intuit.ipp.anywhere.setup({
+           menuProxy: '',
+           grantUrl: '${callbackURL}'
+           });
+         </script>
+         <ipp:connectToIntuit></ipp:connectToIntuit>`;
 
-export default ReportsContainer
+    // then we return it in a format ready for dansgeouslySetInnerHTML
+    return {
+      __html: template
+    };
+  };
+
+  return <div dangerouslySetInnerHTML={QBLogin()} />;
+};
+
+//validate props
+ConnectToQBooksBtn.propTypes = {
+  callbackURL: PropTypes.string.isRequired
+};
+
+export default ReportContainer;
