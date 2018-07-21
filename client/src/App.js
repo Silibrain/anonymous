3d9563f7101e9298e206df1826e22ec3f6c932e1
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-21 17:09:17 -0400

    dependency issues fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import helpers from "./utils/helpers.js";
-import { Home, InventoryAdd, InventoryView, LogIn, Menu, NoMatch, PatientsAdd, PatientsView, PracticionersAdd, PracticionersView, ProceduresAdd, ProceduresExamsView, ReportsMenu, UpdateAccount } from "./pages";
+import { Home, InventoryAdd, InventoryView, LogIn,NoMatch, PatientsAdd, PatientsView, PracticionersAdd, PracticionersView, ProceduresAdd, ProceduresExamsView} from "./pages";
 
 
 
@@ -40,7 +40,7 @@ class App extends Component {
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={LogIn} />
         <Route exact path="/signup" component={LogIn} />
-        <Route exact path="/update" component={UpdateAccount} />
+        {/* <Route exact path="/update" component={UpdateAccount} /> */}
 
         <Route exact path="/home" component={Home} />
 
@@ -54,7 +54,7 @@ class App extends Component {
         <Route exact path="/inventory/view" component={InventoryView} />
         <Route exact path="/procedure/view" component={ProceduresExamsView} />
 
-        <Route exact path="/staff/reports" component={ReportsMenu} />
+        {/* <Route exact path="/staff/reports" component={ReportsMenu} /> */}
 
         <Route component={NoMatch} />
       </Switch>
