9de50147daf05eb5561cff970a5868e89f98ce11
b0bbybaldi <roberto.baldizon@silibrain.com>
2018-07-20 22:34:25 -0400

    beware, do a git pull! I'm force pushing this working version

--- client/src/components/Panels/ReportContainer.js
+++ client/src/components/Panels/ReportContainer.js
@@ -1,39 +1,10 @@
-
 import React from "react";
-/**
- * Connect to QBO Button
- * @param props
- * @returns {XML}
- */
-const ReportContainer = props => {
-  const { callbackURL } = props;
+import "./style.css";
 
-  const QBLogin = () => {
-    // we wrap QBO's button in a template string
-    // becuase it has some stuff that isn't supported by JSX and react
-    // like xml name spaces
-    const template = `
-        <script type="text/javascript" src="https://appcenter.intuit.com/Content/IA/intuit.ipp.anywhere.js"></script>
-         <script>
-         intuit.ipp.anywhere.setup({
-           menuProxy: '',
-           grantUrl: '${callbackURL}'
-           });
-         </script>
-         <ipp:connectToIntuit></ipp:connectToIntuit>`;
+export const ReportsContainer = ({ fluid, children }) => (
+  <div className={`container${fluid ? "-fluid" : ""}`}>
+    {children}
+  </div>
+);
 
-    // then we return it in a format ready for dansgeouslySetInnerHTML
-    return {
-      __html: template
-    };
-  };
-
-  return <div dangerouslySetInnerHTML={QBLogin()} />;
-};
-
-//validate props
-ConnectToQBooksBtn.propTypes = {
-  callbackURL: PropTypes.string.isRequired
-};
-
-export default ReportContainer;
+export default ReportsContainer
