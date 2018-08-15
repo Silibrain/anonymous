4e02e3fbce400333cd7b08d6eb42ee2a0cd28891
Alexandra <alexgood1@gmail.com>
2018-08-15 16:11:27 -0700

    style buttons

--- client/src/components/Panels/NavBar.js
+++ client/src/components/Panels/NavBar.js
@@ -16,26 +16,47 @@ const Nav = () => (
       </div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="/">Hippocrates<span></span>  </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+          <img src="./logo.png" alt="" width="100px" />
+          {/* <a className="navbar-brand" href="/"> */}
+          {/* Hippocrates */}
+          {/* <span />{" "} */}
+          {/* </a> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarsExample05"
+            aria-controls="navbarsExample05"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarsExample05">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
-                <a className="nav-link active" href="/view"> View Cases </a>
+                <a className="nav-link active" href="/view">
+                  {" "}
+                  View Cases{" "}
+                </a>
               </li>
               <li className="nav-item">
-              <a className="nav-link active" href="/add"> Add Case </a>
-              </li> 
+                <a className="nav-link active" href="/add">
+                  {" "}
+                  Add Case{" "}
+                </a>
+              </li>
               <li className="nav-item">
-                <a className="nav-link active" href="/account"> Account </a>
+                <a className="nav-link active" href="/account">
+                  {" "}
+                  Account{" "}
+                </a>
               </li>
             </ul>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   </header>
 );
 
